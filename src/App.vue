a25fcc202b75c5fef6d57efc93c7bf2f1007c0eb
awantech-io <hello.wanlab@gmail.com>
2020-09-14 23:36:10 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,66 @@
+<template>
+  <div id="app">
+    <Todos />
+
+  </div>
+</template>
+
+<script>
+import Todos from './components/Todos';
+
+export default {
+  name: 'App',
+  components: {
+    Todos
+
+  },
+
+  data(){
+    return {
+      todos: [
+        {
+          id:1,
+          title:"Todo One",
+          completed:false
+        },
+        {
+          id:2,
+          title:"Todo Two",
+          completed:false
+        },
+        {
+          id:1,
+          title:"Todo Three",
+          completed:false
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    font-family: Arial, Helvetica, sans-serif;
+    line-height: 1.4;
+  }
+
+  .btn {
+    display: inline-block;
+    border: none;
+    background: #555;
+    color: #fff;
+    padding: 7px 20px;
+    cursor: pointer;
+  }
+
+  .btn:hover {
+    background: #666;
+  }
+</style>
