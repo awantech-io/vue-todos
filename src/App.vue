552e19ba48adc2b63916efc58ad0409f26437b69
awantech-io <hello.wanlab@gmail.com>
2020-09-17 13:36:24 +0800

    header,add new todo item

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,21 @@
 <template>
   <div id="app">
+    <Header />
+    <AddTodo v-on:add-todo="addTodo" />
     <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
   </div>
 </template>
 
 <script>
+import Header from './components/layout/Header';
 import Todos from './components/Todos';
-
+import AddTodo from './components/AddTodo';
 export default {
   name: 'App',
   components: {
-    Todos
+    Header,
+    Todos,
+    AddTodo
 
   },
 
@@ -39,13 +44,17 @@ export default {
   methods:{
     deleteTodo(id) {
       this.todos = this.todos.filter( todo => todo.id !== id );
+    },
+
+    addTodo(newTodo) {
+      this.todos = [...this.todos, newTodo];
     }
   }
 }
 </script>
 
 <style>
- * {
+  * {
     box-sizing: border-box;
     margin: 0;
     padding: 0;
